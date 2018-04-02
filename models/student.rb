7f3aa443f192e679ff8a5fab11f01cf4daca7b22
Antonio Reid <read.musik@gmail.com>
2018-04-02 00:23:42 +0000

    automatically backed up by learn

--- models/student.rb
+++ models/student.rb
@@ -1,11 +1,12 @@
-class Student 
+class Student
 
   attr_reader :name, :grade
 
   STUDENTS = []
 
   def initialize(params)
-    
+    @name = params[:name]
+    @grade = params[:grade]
   end
 
 end
